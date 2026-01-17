842bf012f12e6667eeeb41be6884ea2f6e8790a0
samah.moumen <samah_moumen@um5.ac.ma>
2026-01-18 00:24:51 +0100

    first

---
+++ app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var _ = os.Args
+var _ = exec.Command
+
+func main() {
+	command := os.Args[3]
+	args := os.Args[4:len(os.Args)]
+
+	cmd := exec.Command(command, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Err: %v", err)
+		os.Exit(1)
+	}
+
+}
